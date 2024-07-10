143d3c8c95b8e4d1879b612a8a575c0ba471bbf1
ali <alinajarzadegan1383@gmail.com>
2024-07-10 21:00:02 +0330

    Next button saves the ship cordinances in a text file

--- drg/main.cpp
+++ drg/main.cpp
@@ -13,6 +13,7 @@
 #include <QDebug>
 #include <QPushButton>
 #include <QSize>
+#include <fstream>
 
 class Ship {
 public:
@@ -119,12 +120,27 @@ void Board::setArray(std::vector<std::pair< Ship*,QLabel*>> sshhips)
 
         if(sh.second->x()>= 250*0.8 && sh.second->x() <= 860*0.8 && sh.second->y() >= 220*0.8 && sh.second->y() <= 834*0.8)
         {
+            std::ofstream outFile("cordShips.txt");
+
             if(sh.first->st == "h")
             {
                 for(int i = (sh.second->x()-200)/48; i < ((sh.second->x()-200)/48) + size; i++)
                 {
                     table[(sh.second->y()-183)/48][i] = 'S';
+
+                    outFile << (sh.second->y()-183)/48 << "," << (sh.second->x()-200)/48 << '\n';
                 }
+                outFile << 'n';
+            }
+            if(sh.first->st == "v")
+            {
+                for(int i = (sh.second->y()-200)/48; i < ((sh.second->y()-200)/48) + size; i++)
+                {
+                    table[i][(sh.second->x()-183)/48] = 'S';
+
+                    outFile << (sh.second->y()-183)/48 << "," << (sh.second->x()-200)/48 << '\n';
+                }
+                outFile << 'n';
             }
         }
     }
